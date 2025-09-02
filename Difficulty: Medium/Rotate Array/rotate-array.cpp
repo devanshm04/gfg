797ecdd1a63a56c24b7d462d34c03e3e55fe9f66
Devansh Maheshwari <130562094+devanshm04@users.noreply.github.com>
2025-09-02 16:13:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(vector<int>& arr, int d) {
+        // code here
+        
+        int n=arr.size();
+        d=d%n;
+        vector<int>temp(n);
+        for(int i=0;i<n;i++){
+            temp[(i-d+n)%n]=arr[i];
+        }
+        arr=temp;
+    }
+};
